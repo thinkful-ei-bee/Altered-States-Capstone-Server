35ff215ab5b6622fdc3b6bfc1a7c784449a8628f
Enrique Montemayor <eemontemayor@live.com>
2019-06-05 17:13:37 -0500

    DB_Url

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-  DATABASE_URL: process.env.DATABASE_URL, 
+  // DATABASE_URL: process.env.DATABASE_URL, 
+  DB_URL: process.env.DB_URL, 
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
 };
--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@
 
 const app = require('./app');
 const knex = require('knex');
-const { PORT, DATABASE_URL } = require('./config');
+const { PORT, DB_URL, DATABASE_URL } = require('./config');
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL
+  connection: DB_URL
 });
 
 app.set('db', db);
