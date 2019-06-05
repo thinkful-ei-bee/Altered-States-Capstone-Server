df1480191f3a20342f6478bdac01d29dfda7708e
Scott Williams <scottcolemanwilliams@gmail.com>
2019-06-05 13:12:10 -0500

    config secret

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
   DATABASE_URL: process.env.DATABASE_URL, 
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
