f372b6e6fe4e36eeb8629241cc66a5493234a3b3
Loxphordex <silasishallahan@gmail.com>
2019-06-11 15:27:58 -0700

    working

--- src/entry/entry-services.js
+++ src/entry/entry-services.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const xss = require('xss');
+
 const EntryServices = {
 
   getById(db, id) {
@@ -23,6 +25,32 @@ const EntryServices = {
       .select('*')
       // .where('entry.user_id', id);
       .where('user_id', id);
+  },
+
+  serializeEntry(entry) {
+    return {
+      id: entry.id,
+      date_created: entry.date_created,
+      text: xss(entry.text),
+      happiness: entry.happiness,
+      face_url: entry.face_url,
+      Joy: entry.Joy,
+      Fear: entry.Fear,
+      Sadness: entry.Sadness,
+      Anger: entry.Anger,
+      Analytical: entry.Analytical,
+      Confident: entry.Confident,
+      Tentative: entry.Tentative,
+      face_anger: entry.face_anger,
+      face_contempt: entry.face_contempt,
+      face_disgust: entry.face_disgust,
+      face_fear: entry.face_fear,
+      face_happiness: entry.face_happiness,
+      face_neutral: entry.face_neutral,
+      face_sadness: entry.face_sadness,
+      face_surprise: entry.face_surprise,
+      user_id: entry.user_id
+    };
   }
 };
 
